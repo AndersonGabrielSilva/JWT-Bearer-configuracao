df8cf943d00a3e6531e601f2d4463302008dbe36
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2021-03-07 13:33:20 -0300

    first commit

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Linq;
+using Shop.Models;
+
+namespace Shop.Repositories
+{
+    public static class UserRepository
+    {
+        public static User Get(string username, string password)
+        {   
+            var users = new List<User>();
+
+            users.Add(new User {Id = 1, Username = "anderson", Password = "anderson"});
+            users.Add(new User {Id = 2, Username = "gabriel", Password = "gabriel"});
+
+            return users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password.ToLower() == password.ToLower()).FirstOrDefault();
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,7 @@
+namespace Shop
+{
+    public static class  Settings
+    {
+        public static string Secret = "@#$%&*%(oswogibow65+4+//*-.-agwe6.2.2.2.wgwqg-@%&*¨*@$&)+*(%$$#%#";
+    }
+}
